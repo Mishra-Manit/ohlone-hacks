83dafb79a472ed2799ed8d1923a1ec9f4975623e
laneric <ericlan707@gmail.com>
2024-03-13 11:05:09 -0700

    setup page

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+
+export default function Footer() {
+    return (
+        <div className="mx-10 font-mono">
+            <div className="flex flex-col sm:flex-row justify-between mt-48">
+                <text>
+                    ERIC LAN
+                </text>
+                <text>
+                    IRVINGTON COMPUTER SCIENCE CLUB
+                </text>
+                <text>
+                    IRVINGTON HACK CLUB
+                </text>
+                <text>
+                    &copy; 2024
+                </text>
+            </div>
+            {/* TODO: use image instead of text */}
+            <Image
+                src="/images/VIKINGHACKS_TEXT.png"
+                className="hidden sm:block"
+                alt="Viking Hacks"
+                width={1881}
+                height={215}
+            />
+            <Image
+                src="/images/VIKINGHACKS_COL_TEXT.png"
+                className="block sm:hidden"
+                alt="Viking Hacks"
+                width={1881}
+                height={215}
+            />
+        </div>
+    )
+}
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import StyledLink from "./StyledLink";
+import Link from 'next/link';
+
+const navItems = {
+	'/': {
+		name: 'HOME',
+	},
+	'/ohlone24': {
+		name: 'REGISTER',
+	},
+};
+export default function Header() {
+	return (
+		<div className="w-full px-10 py-6 border-b shadow-[0_0_15px_0_rgba(0,0,0,0.1)] font-mono font-light flex justify-between items-center">
+			<div className="hidden sm:block">
+				VIKING HACKS
+			</div>
+			<div>
+				<Image
+					alt="Viking Hacks Logo"
+					src="/images/VH_SINGLE_LOGO_BLACK.png"
+					className="w-6 select-none"
+					width={624}
+					height={769}
+				/>
+			</div>
+			<div className="flex gap-5">
+				{Object.entries(navItems).map(([path, { name }]) => {
+					return (
+						<StyledLink key={path} href={path} target={"_self"} text={name} arrow={false} />
+					);
+				})}
+			</div>
+		</div>
+	)
+}
