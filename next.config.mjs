b1fac1a456d0d76e1082f820174276eaa5fb3206
laneric <ericlan707@gmail.com>
2024-03-17 23:24:19 -0700

    redirect prospectus to files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/prospectus',
+                destination: '/files/prospectus.pdf',
+                permanent: true,
+            },
+        ]
+    },
+};
 
 export default nextConfig;
